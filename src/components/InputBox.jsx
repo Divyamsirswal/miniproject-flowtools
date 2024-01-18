ec3c4bfe95af8c98056fedef1d0c37d27680d4ec
divyamTest <divyamsirswal361@gmail.com>
2024-01-18 14:21:02 +0530

    changes

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -28,16 +28,14 @@ function InputBox({
           placeholder="Amount"
           disabled={amountDisable}
           value={amount}
-          onChange={(e) =>
-            onAmountChange && onAmountChange(Number(e.target.value))
-          }
+          onChange={(e) => onAmountChange(Number(e.target.value))}
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
           value={selectCurrency}
-          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          onChange={(e) => onCurrencyChange(e.target.value)}
           disabled={currencyDisable}
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
         >
