3966b35ece72055019112ea192498896b68102f2
divyamTest <divyamsirswal361@gmail.com>
2024-01-19 17:05:21 +0530

    changes in file homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,28 +6,28 @@ const HomePage = () => {
   return (
     <div className="relative w-screen h-screen bg-black">
       <div className="z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-stone-950 w-10/12 h-5/6 border rounded-5xl rounded-tl-[0vw] rounded-tr-[10rem] rounded-bl-[10rem] rounded-br-[0vw] rounded-lg shadow-lg flex flex-col items-center justify-center">
-        {/* main heading */}
+        
         <div className="flex items-baseline justify-center">
           <h3 className="text-5xl font-sf_li text-white mb-10">flow</h3>
           <h5 className="text-1xl font-sf_li text-white">tools</h5>
         </div>
 
         <MediaQuery maxDeviceWidth={414}>
-          {/* Mobile Styles */}
+          
           <div className="relative w-11/12 h-3/5 flex-col gap-5 bg-transparent border rounded-3xl flex items-center justify-center">
             {renderButtons("text-lg", "h-16", "py-2")}
           </div>
         </MediaQuery>
 
         <MediaQuery minDeviceWidth={415} maxDeviceWidth={896}>
-          {/* Tablet Styles */}
+         
           <div className="relative w-3/4 h-4/5 flex-col gap-5 bg-transparent border rounded-3xl flex items-center justify-center">
             {renderButtons("text-2xl", "h-20", "py-4")}
           </div>
         </MediaQuery>
 
         <MediaQuery minDeviceWidth={897}>
-          {/* PC/Laptop Styles */}
+         
           <div className="relative w-1/4 h-3/5 flex-col gap-5 bg-transparent border rounded-3xl flex items-center justify-center">
             {renderButtons("text-2xl", "h-16", "py-4")}
           </div>
